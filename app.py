7cd2b9d57be5b81ef81babf90dc5ee537ae2954b
Joshua Li <joshua.li@gmail.com>
2026-02-12 09:55:00 -0500

    test

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ except Exception:
 client = OpenAI(api_key=api_key)
 
 st.set_page_config(page_title="Data Analysis Tool", layout="wide")
-st.title("Data Analysis App TEST")
+st.title("Data Analysis App ABC")
 
 df = pd.read_csv('movies.csv')
 
